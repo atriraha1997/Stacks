2c77784ea8b2e22d921fc2ed2630101316a8f7b0
atriraha1997 <130207920+atriraha1997@users.noreply.github.com>
2023-04-22 19:52:07 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int topIndex = 0;
+        int currArea = 0;
+        int maxArea = 0;
+        for(int i=0;i<heights.length;i++){
+            while(!(stack.isEmpty()) && heights[stack.peek()]>=heights[i]){
+                topIndex = stack.pop();
+                currArea = heights[topIndex]*(stack.isEmpty()? i: i-stack.peek()-1);
+                maxArea = Math.max(currArea,maxArea);
+            }
+            stack.push(i);
+        }
+        while(!(stack.isEmpty())){
+            topIndex = stack.pop();
+            currArea = heights[topIndex]*
+            (stack.isEmpty()? heights.length: heights.length-stack.peek()-1);
+            maxArea = Math.max(currArea,maxArea);
+        }
+        return maxArea;
+    }
+}
